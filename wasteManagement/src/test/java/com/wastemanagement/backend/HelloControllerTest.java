70e34120b5bdc38b89f81ea849df8c01046235ec
Fatma Rekik <rekikfatma01@gmail.com>
2025-10-29 17:11:17 +0100

    added /hello endpoint with HelloController and unit test

---
+++ wasteManagement/src/test/java/com/wastemanagement/backend/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.wastemanagement.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+}
