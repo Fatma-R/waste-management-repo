2d98c21843ecdd8c642cd93232db73a831c0c5c9
Fatma Rekik <rekikfatma01@gmail.com>
2025-11-29 16:19:55 +0100

    Back: added missing annotations

--- wasteManagement/src/main/java/com/wastemanagement/backend/repository/tournee/TourneeAssignmentRepository.java
+++ wasteManagement/src/main/java/com/wastemanagement/backend/repository/tournee/TourneeAssignmentRepository.java
@@ -2,6 +2,8 @@ package com.wastemanagement.backend.repository.tournee;
 
 import com.wastemanagement.backend.model.tournee.TourneeAssignment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TourneeAssignmentRepository extends MongoRepository<TourneeAssignment, String> {
 }
